7092ca57cf202387792deb170e0270f3d40fff69
RiazK <RiazCKhan@gmail.com>
2021-07-22 18:46:07 -0400

    Tail Array Function

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😇 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let newArray = '';
+  for (let i = 0; i < array.length; i++) {
+    newArray = newArray + array[i];
+  }
+  newArray = newArray.slice(1);
+  return newArray;
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
