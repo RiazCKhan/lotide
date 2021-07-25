5116f1a10ebea2b58fad5ae60f216740f686bda4
RiazK <RiazCKhan@gmail.com>
2021-07-25 12:40:45 -0400

    Strict Equality Arrays Function

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😇 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
