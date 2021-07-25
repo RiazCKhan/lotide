349320719538f0c1fbe5b226e62e2f61dc9bf656
RiazK <RiazCKhan@gmail.com>
2021-07-25 15:01:41 -0400

    Compare and Remove Array Element Function

---
+++ without.js
@@ -0,0 +1,34 @@
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`😇 Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🤬 Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false
+  }
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+};
+
+const without = function(original, remove) {
+  let result = [];
+  for (i = 0; i < original.length; i++) {
+    if (original[i] !== remove[i]) {
+      result.push(original[i])
+    }
+  }
+  // console.log(result)
+  return result;
+}
+
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
