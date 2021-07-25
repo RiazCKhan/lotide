157653df108839c1dd8f1b35c17f6ad5afd9dca9
RiazK <RiazCKhan@gmail.com>
2021-07-25 15:02:50 -0400

    applied eslint

--- without.js
+++ without.js
@@ -8,27 +8,27 @@ const assertArraysEqual = function(arrOne, arrTwo) {
 
 const eqArrays = function(arrOne, arrTwo) {
   if (arrOne.length !== arrTwo.length) {
-    return false
+    return false;
   }
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        return false;
-      }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 };
 
 const without = function(original, remove) {
   let result = [];
   for (i = 0; i < original.length; i++) {
     if (original[i] !== remove[i]) {
-      result.push(original[i])
+      result.push(original[i]);
     }
   }
   // console.log(result)
   return result;
-}
+};
 
 
-without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
