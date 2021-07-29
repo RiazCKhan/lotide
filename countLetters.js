9bd10c2644099c5f4adb527ab4f1d493fb20ebda
RiazK <RiazCKhan@gmail.com>
2021-07-29 17:16:33 -0400

    Function that will count number of Letters in a String

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😇 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  // is a second binding needed?
+  for (const letter of string) {
+    if (!result[letter]) {
+      result[letter] = 1;
+    } else {
+      result[letter] += 1;
+    }
+  }
+  return result;
+};
