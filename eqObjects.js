1f9cfd2c8ad9f8c61f0522821b673d6bb53d5688
RiazK <RiazCKhan@gmail.com>
2021-07-30 09:13:13 -0400

    eqObjects understanding  Primitive Test Cases

---
+++ eqObjects.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😇 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both obj have same number of keys
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1) === Object.keys(object2)) {
+    return false; // if obj(s) do not have the same num of keys exit process
+  }
+  for (let keys in object1) {
+// console.log(object1, object2)
+    if (object1[keys] === object2[keys]) {
+// console.log(object1[keys], 'obj 1 keys') -- check IF statement hits
+// console.log(object1[keys], 'obj 2 keys') -- AND obj keys are received
+      return true; // if obj(s) have the same key values return true
+    }
+  }
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+// console.log(assertEqual(ab, abc)); // => false
+
+// Step 2 primitives test case console.log(eqObjects(ab, ba))
