93d176b987bd6288ac5acffae289630e0097ee42
RiazK <RiazCKhan@gmail.com>
2021-08-01 21:09:12 -0400

    Map func with anony and callback incl test

---
+++ map.js
@@ -0,0 +1,44 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1)
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`😇 Assertion Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🤬 Assertion Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  if (arrOne.length === arrTwo.length) {
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const test1 = ['blue', 'red', 'green']
+const test2 = ['g', 'c', 't', 'm', 't']
+const test3 = ['l', 'm', 'n', 'o']
+const test4 = [1, 2, 3, 4, 5] 
+
+console.log(assertArraysEqual(results1, test1));
+console.log(assertArraysEqual(results1, test2));
+console.log(assertArraysEqual(results1, test3));
+console.log(assertArraysEqual(results1, test4));
