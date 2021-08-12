f46c15d975025cdecabd7cc0dd336fedfaec8405
RiazK <RiazCKhan@gmail.com>
2021-08-12 14:00:55 -0400

    test cases updated with mocha chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,27 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual'); // RCK built assertion
+
+const assert = require('chai').assert;
 const head = require('../head');
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([4, 1, 6]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
-assertEqual(head(["gym", "shark", "Jim"]), "gym");
+describe("It should return the first element of an arr", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 6 for [6, 7, 8]", () => {
+    assert.strictEqual(head([6, 7, 8]), 6);
+  });
+
+  it("returns 4 for [4, 1, 6]", () => {
+    assert.strictEqual(head([4, 1, 6]), 4);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+});
