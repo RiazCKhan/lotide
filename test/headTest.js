c549bf1abb5e444b1f58d988d039293f310eca2b
RiazK <RiazCKhan@gmail.com>
2021-08-12 09:37:43 -0400

    added more test cases

--- test/headTest.js
+++ test/headTest.js
@@ -3,4 +3,7 @@ const head = require('../head');
 
 // TEST CODE
 assertEqual(head([5, 6, 7]), 5);
+assertEqual(head([4, 1, 6]), 6);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+assertEqual(head(["gym", "shark", "Jim"]), "gym");
