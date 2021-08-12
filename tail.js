8feca68f72e83cac94864cda9539d2deb4b6c1ae
RiazK <RiazCKhan@gmail.com>
2021-08-11 22:25:32 -0400

    added module export, removed test cases, and refractor

--- tail.js
+++ tail.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😇 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const tail = function(array) {
+const tail = (array) => {
   let newArray = '';
   for (let i = 0; i < array.length; i++) {
     newArray = newArray + array[i];
@@ -15,8 +9,4 @@ const tail = function(array) {
   return newArray;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
+module.exports = tail;
