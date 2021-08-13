474e70f0a6561cf0ea5c70e808e65a6eb9812445
RiazK <RiazCKhan@gmail.com>
2021-08-13 10:01:09 -0400

    reviewed and revised for final submission

--- findKey.js
+++ findKey.js
@@ -6,7 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// ----- Refractor Solution
+// ----- Refactored Solution
 // const findKey = (object, callback) => {
 //   let keys = Object.keys(object)
 //   for (let key of keys) {
@@ -17,9 +17,10 @@ const assertEqual = function(actual, expected) {
 //   return undefined
 // }
 
-// ----- Original Solution - findKeyByValue.js referenced for solution
+// ----- Original Solution - for clarity - findKeyByValue.js referenced for solution
 const findKey = function(object, callback) {
-  for (let key of Object.keys(object)) { // This will loop through the outter key values of each key, i.e., Blue Hill (key), {(value)}
+ const keys = Object.keys(source) // declared to clarify what is being viewed in the for of loop
+  for (let key of keys) { // This will loop through the outter key values of each key, i.e., Blue Hill (key), {(value)}
     if (callback(object[key])) { // This will pass the inner object key/value pair, i.e., stars (key), number (value) to the callback function
       return key
     }
@@ -36,6 +37,13 @@ const source = {
   "Akelarre":  { stars: 3 }
 };
 
-results1 = findKey(source, data => data.stars === 2); // will review object[key] to determine if the data.stars (the value of the key) is equal to 2
-// console.log(results1)
-// Expected Output "noma"
+
+// Test CASE 1:
+const results1 = findKey(source, data => data.stars === 2); // will review object[key] to determine if the data.stars (the value of the key) is equal to 2
+console.log(results1) // Expected Output "noma"
+assertEqual(results1, "noma");
+
+// TEST CASE 2:
+const results2 = findKey(source, data => data.stars === 0);
+assertEqual(results2, undefined);
+
